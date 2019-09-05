2f32eca261cf2773e6630e1b6e536e751155ca3c
David Ahlstom <david0673@gmail.com>
2019-09-04 18:43:33 -0700

    got first test to work

--- test/user.test.js
+++ test/user.test.js
@@ -11,12 +11,8 @@ describe("user.test.js", () => {
 
         const res = await chai
         .request(app)
-        .get(`/user`)
-<<<<<<< HEAD
-
-=======
+        .get(`/auth/profile`)
         .set("Authorization", `Bearer ${token}`)
->>>>>>> c33ccb311d9983e735aa43f587a6645299440842
 
         expect(res.status).to.eq(200)
     })
