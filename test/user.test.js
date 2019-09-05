32f1abdd7899a63d798c480fd55fdfc0e297aff9
anthony <anthonyt.wallace@icloud.com>
2019-09-04 18:53:25 -0700

    added invalid check to profile test

--- test/user.test.js
+++ test/user.test.js
@@ -5,7 +5,7 @@ const { app } = require("../src/server")
 const { signUp, login } = require("./auth.test")
 
 describe("user.test.js", () => {
-    it("GET /user/:_id", async () => {
+    it("GET /user/: Should allow a valid user to view thier profile", async () => {
         const user = await login()
         const token = user.body.token
 
@@ -15,5 +15,17 @@ describe("user.test.js", () => {
         .set("Authorization", `Bearer ${token}`)
 
         expect(res.status).to.eq(200)
+    })
+    
+    it("GET /user/: Should not allow an invalid user to view a profile", async () => {
+        const user = await login()
+        const token = "Not a valid token"
+
+        const res = await chai
+        .request(app)
+        .get(`/auth/profile`)
+        .set("Authorization", `Bearer ${token}`)
+
+        expect(res.status).to.eq(401)
     })
 })
