b2e4d5a7e5367f9d43d6f253eecb4bb471f11b0e
David Ahlstom <david0673@gmail.com>
2019-09-05 19:26:52 -0700

    added user book checking out

--- src/routes/book.route.js
+++ src/routes/book.route.js
@@ -12,8 +12,8 @@ router.get("/", async (req, res) => {
   const books = await Book.find();
   const available = []
 
-  for(book in books) {
-    if(!books[book].checkedout){
+  for (book in books) {
+    if (!books[book].checkedout) {
       available.push(books[book])
     }
   }
@@ -32,10 +32,10 @@ router.get("/all", async (req, res) => {
 router.post("/", jwtMiddleware, async (req, res) => {
   const errors = validationResult(req);
 
-  if(!req.user.admin ) return res.sendStatus(401);
+  if (!req.user.admin) return res.sendStatus(401);
 
   const { booktitle, author, checkedout } = req.body;
-  
+
   const book = new Book({ booktitle, author, checkedout });
   await book.save();
 
@@ -47,18 +47,18 @@ router.get("/:_id", async (req, res) => {
   const { _id } = req.params;
   const book = await Book.findOne({ _id }).populate(["username"]);
 
-  if(!book) return res.sendStatus(404);
+  if (!book) return res.sendStatus(404);
 
   res.send(book);
 });
 
 // update 
-router.patch("/:_id",  jwtMiddleware, async (req, res) => {
+router.patch("/:_id", jwtMiddleware, async (req, res) => {
   const { _id } = req.params;
   const book = await Book.findOne({ _id });
 
-  if(!book) return res.sendStatus(404);
-  if(!req.user.admin ) return res.sendStatus(401);
+  if (!book) return res.sendStatus(404);
+  if (!req.user.admin) return res.sendStatus(401);
 
 
   book.booktitle = req.body.booktitle;
@@ -69,13 +69,40 @@ router.patch("/:_id",  jwtMiddleware, async (req, res) => {
   res.send(book);
 });
 
+
+// update user checks out book 
+router.patch("/checkout/:_id", jwtMiddleware, async (req, res) => {
+  const { _id } = req.params;
+  const book = await Book.findOne({ _id });
+
+  if (!book) return res.sendStatus(404);
+  if (!req.user) return res.sendStatus(401);
+
+
+  book.booktitle = req.body.booktitle;
+  book.author = req.body.author
+  if (!book.checkedout) {
+    book.user = req.user
+    book.checkedout = !book.checkedout
+  } else {
+    book.user = ""
+    book.checkedout = !book.checkedout
+  }
+
+
+
+  await book.save();
+
+  res.send(book);
+});
+
 // delete
 router.delete("/:_id", jwtMiddleware, async (req, res) => {
   const { _id } = req.params;
-  const book= await Book.findOne({ _id });
-  
-  if(!book) return res.sendStatus(404);
-  if(!req.user.admin) return res.sendStatus(401);
+  const book = await Book.findOne({ _id });
+
+  if (!book) return res.sendStatus(404);
+  if (!req.user.admin) return res.sendStatus(401);
 
   await book.remove();
 
